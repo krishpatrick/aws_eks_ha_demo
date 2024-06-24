7164ee2aea3dfe841fa90159131a42d901addd5a
Krishnakumar Nandhakumar <krishnakumar.nandhakumar@ing.com>
2024-06-25 01:29:46 +0200

    S3 bucket and variables

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,14 +2,6 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-variable "cluster_name" {
-  default = "demo-nginx-cl-01"
-}
-
-variable "cluster_version" {
-  default = "1.30"
-}
-
 terraform {
   required_providers {
     aws = {
