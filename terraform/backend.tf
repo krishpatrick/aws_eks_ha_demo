77df2039b1447979a10e635d284ba2af1fd16659
Krishnakumar Nandhakumar <krishnakumar.nandhakumar@ing.com>
2024-06-25 15:19:44 +0200

    Update backend

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">=0.12.0"
+  backend "s3" {
+    region  = "eu-central-1"
+    profile = "default"
+    key     = "demo-nginx-cl-02-terraformstatefile"
+    bucket  = "nginxdemobucket1"
+  }
+}
